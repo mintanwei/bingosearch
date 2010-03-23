8e676e30f5eb11548d80f98c1f1aee8b0d31fe3b
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-03-23 11:18:33 +0000

    the crawler

---
+++ trunk/crawler/crawler.h
@@ -0,0 +1,22 @@
+#ifndef BINGO_CRAWLER_CRAWLER_H
+#define BINGO_CRAWLER_CRAWLER_H
+
+namespace bingo 
+{
+namespace crawler 
+{
+
+class Crawler 
+{
+public:
+    Crawler();
+    virtual ~Crawler();
+
+private:
+    
+};
+
+} // end of crawler 
+} // end of bingo 
+
+#endif // end of include guard: BINGO_CRAWLER_CRAWLER_H 
