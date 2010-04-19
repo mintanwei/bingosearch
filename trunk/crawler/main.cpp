057348788f0ef0d11d8cc15624ca0f0c122c7b72
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-19 03:01:47 +0000

    go

---
+++ trunk/crawler/main.cpp
@@ -0,0 +1,18 @@
+/** 
+ * @file main.cpp
+ * @brief 
+ * @author Chao Jiansong, jiansong.chaojs@gmail.com
+ * @version 0.1
+ * @date 2010-03-27
+ */
+
+#include "crawler.h"
+
+using namespace bingo::crawler;
+
+int main(int argc, const char *argv[])
+{
+    Crawler crawler;   
+    crawler.Start();
+    return 0;
+}
