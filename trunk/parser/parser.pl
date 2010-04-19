b867f247779f429095a0f4db360f7dd903fd3736
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-19 03:05:01 +0000

    go

---
+++ trunk/parser/parser.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $file = "../data/query_list";
+open QUERY, $file or die "FAIL TO OPEN $file";
+
+while (<QUERY>) {
+	chomp(my $tmp = $_);
+	my $file = "../data/meta_data/$tmp";
+	open OUTPUT, ">$file" or die "FAIL TO OPEN $file";
+	foreach (0...49) {
+		my $file = "../data/pages/$tmp/$_";
+		open NOW, $file or die "FAIL TO OPEN $file";
+		while (<NOW>) {
+			while (/(\["\/imgres\?.*?""])/g) {
+				my $tmp = $1;
+				# img url
+				if ($tmp =~ /imgurl\\x3d(.*?)\\x26/) {
+					print OUTPUT "$1\n";
+				}
+				# img ref url
+				if ($tmp =~ /imgrefurl\\x3d(.*?)\\x26/) {
+					print OUTPUT "$1\n";
+				}
+				# img size & img format
+				if ($tmp =~ /"(\d+ x \d+ - \d+k)","(.*?)"/) {
+					print OUTPUT "$1\n$2\n";
+				}
+			}
+		}
+		close NOW;
+	}
+	close OUTPUT;
+}
+
+close QUERY;
