f42e042cf5b1490b1e16a257bcf148ed4b983131
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-19 03:05:13 +0000

    go

---
+++ trunk/visual_rank/visual_rank.h
@@ -0,0 +1,23 @@
+/** 
+ * @file visual_rank.h
+ * @brief 
+ * @author Chao Jiansong, jiansong.chaojs@gmail.com
+ * @version 0.1
+ * @date 2010-04-06
+ */
+
+#ifndef BINGO_VISUAL_RANK_VISUAL_RANK_H
+#define BINGO_VISUAL_RANK_VISUAL_RANK_H
+
+namespace bingo 
+{
+namespace visualrank 
+{
+
+} /* end of visualrank */
+} /* end of bingo */
+
+
+
+#endif /* end of include guard: BINGO_VISUAL_RANK_VISUAL_RANK_H */
+
