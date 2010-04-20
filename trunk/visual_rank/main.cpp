9ae4d08f54410a5b7094d5e9efb1a92611272aea
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-20 04:30:28 +0000

    go

---
+++ trunk/visual_rank/main.cpp
@@ -0,0 +1,17 @@
+/** 
+ * @file main.cpp
+ * @brief 
+ * @author Chao Jiansong, jiansong.chaojs@gmail.com
+ * @version 0.1
+ * @date 2010-04-19
+ */
+
+#include "visual_rank.h"
+
+using namespace bingo::visualrank;
+
+int main(int argc, const char *argv[])
+{
+	
+	return 0;
+}
