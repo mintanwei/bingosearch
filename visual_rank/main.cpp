c28a7e79b62cc0673c50b6f9b4b13bc682892493
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-23 13:58:18 +0000

    go

--- visual_rank/main.cpp
+++ visual_rank/main.cpp
@@ -7,18 +7,32 @@
  */
 
 #include "visual_rank.h"
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
 
+using namespace std;
 using namespace bingo::visualrank;
 using namespace bingo::imagesimilarity;
 
 int main(int argc, const char *argv[])
 {
+	ifstream in("../data/query_list");
+
+	if (in.fail()) 
+	{
+		cerr << "FAIL TO OPEN FILE ../data/query_list" << endl;
+		exit(1);
+	}
+
+	string query;
 	ImageSimilarity* judge = new ImageSimilarity();
 	VisualRank visualRank;
-
-	visualRank.GetVisualRank("../data/images/camera/", judge);
-	visualRank.Output("../data/visual_rank/camera");
+	while (in >> query) 
+	{
+		visualRank.GetVisualRank(string("../data/images/") + query + "/", judge);
+		visualRank.Output(string("../data/visual_rank/") + query);
+	}
 
 	delete judge;
 
