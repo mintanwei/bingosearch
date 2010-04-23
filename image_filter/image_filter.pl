c32e44f07370753b235b49d9801cb080836c9940
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-23 14:25:14 +0000

    go

--- image_filter/image_filter.pl
+++ image_filter/image_filter.pl
@@ -9,6 +9,9 @@ open QUERY, $file or die "FAIL TO OPEN $file";
 while (<QUERY>) {
 	chomp();
 	my $real_id = 0;
+	my $meta_data = "../data/meta_data/$_";
+	open METADATA, "$meta_data" or die "FAIL TO OPEN $meta_data";
+	open TMP, ">$meta_data"."_tmp" or die "FAIL TO OPEN $meta_data"."_tmp";
 	for (my $var = 0; $var < 1000; $var++) {
 		$file = "../data/images/$_/$var";
 		my $file2 = "../data/images/$_/real$real_id";
@@ -16,6 +19,10 @@ while (<QUERY>) {
 			my $size = -s "$file";
 			if ($size != 0) {
 				rename("$file", "$file2");
+				for (my $var2 = 0; $var2 < 4; $var2++) {
+					my $line = <METADATA>;
+					print TMP "$line";
+				}
 				++ $real_id;
 			}
 			else {
@@ -28,6 +35,9 @@ while (<QUERY>) {
 		my $file2 = "../data/images/$_/$var";
 		rename("$file", "$file2");
 	}
+	rename("$meta_data"."_tmp", "$meta_data");
+	close TMP;
+	close METADATA;
 }
 
 close QUERY;
