82e687b3babf18e31751c5bb70e1b83c710ad720
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-27 06:26:43 +0000

    go

--- parser/parser.pl
+++ parser/parser.pl
@@ -7,37 +7,37 @@ my $file = "../data/query_list";
 open QUERY, $file or die "FAIL TO OPEN $file";
 
 while (<QUERY>) {
-	chomp(my $tmp = $_);
-	my $file = "../data/meta_data/$tmp";
-	open OUTPUT, ">$file" or die "FAIL TO OPEN $file";
-	foreach (0...49) {
-		my $file = "../data/pages/$tmp/$_";
-		open NOW, $file or die "FAIL TO OPEN $file";
-		while (<NOW>) {
-			while (/(\["\/imgres\?.*?""])/g) {
-				my $tmp = $1;
+    chomp(my $tmp = $_);
+    my $file = "../data/meta_data/$tmp";
+    open OUTPUT, ">$file" or die "FAIL TO OPEN $file";
+    foreach (0...49) {
+        my $file = "../data/pages/$tmp/$_";
+        open NOW, $file or die "FAIL TO OPEN $file";
+        while (<NOW>) {
+            while (/(\["\/imgres\?.*?""])/g) {
+                my $tmp = $1;
                 print "$tmp\n\n\n";
                 # thumbnail
                 if ($tmp =~ /" target\\x3d_blank","(.*?)".*?(http:\/\/t\d.gstatic.com\/images)/) {
                     print OUTPUT "$2?q=tbn:$1\n";
                 }
-				# img url
-				if ($tmp =~ /imgurl\\x3d(.*?)\\x26/) {
-					print OUTPUT "$1\n";
-				}
-				# img ref url
-				if ($tmp =~ /imgrefurl\\x3d(.*?)\\x26/) {
-					print OUTPUT "$1\n";
-				}
-				# img size & img format
-				if ($tmp =~ /"(\d+ x \d+ - \d+k)","(.*?)"/) {
-					print OUTPUT "$1\n$2\n";
-				}
-			}
-		}
-		close NOW;
-	}
-	close OUTPUT;
+                # img url
+                if ($tmp =~ /imgurl\\x3d(.*?)\\x26/) {
+                    print OUTPUT "$1\n";
+                }
+                # img ref url
+                if ($tmp =~ /imgrefurl\\x3d(.*?)\\x26/) {
+                    print OUTPUT "$1\n";
+                }
+                # img size & img format
+                if ($tmp =~ /"(\d+ x \d+ - \d+k)","(.*?)"/) {
+                    print OUTPUT "$1\n$2\n";
+                }
+            }
+        }
+        close NOW;
+    }
+    close OUTPUT;
 }
 
 close QUERY;
