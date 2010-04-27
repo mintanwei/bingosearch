9cd0d8e83ece5a397a25bbe8764d42b20269c62b
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-27 06:05:16 +0000

    go

---
+++ trunk/go.sh
@@ -0,0 +1,12 @@
+cd crawler
+./crawler
+cd ../parser/
+./parser.pl
+cd ../image_downloader/
+./image_downloader.pl
+cd ../image_filter/
+./image_filter.pl
+cd ../visual_rank/
+./visual_rank
+cd ../index_builder/
+./index_builder
