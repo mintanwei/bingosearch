b82653732d842d959151a840641b89406255e8f7
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-29 07:15:05 +0000

    go

--- trunk/visual_rank/build.sh
+++ trunk/visual_rank/build.sh
@@ -1,1 +1,1 @@
-g++ -o visual_rank main.cpp visual_rank.cpp image_similarity.cpp -I ../common/ -DLOG_LEVEL=0
+g++ -o visual_rank main.cpp visual_rank.cpp image_similarity.cpp image_similarity_histogram.cpp -I ../common/ -DLOG_LEVEL=0
