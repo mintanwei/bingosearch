b4af99d80be4bb6428c722536a761758bcc08dcc
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-04-29 07:25:16 +0000

    go

--- visual_rank/image_similarity_histogram.cpp
+++ visual_rank/image_similarity_histogram.cpp
@@ -1,7 +1,7 @@
 /** 
  * @file image_similarity_histogram.cpp
  * @brief 
- * @author WangMing, chris.wangm@alibaba-inc.com
+ * @author Chao Jiansong, jiansong.chao@gmail.com
  * @version 0.1
  * @date 2010-04-28
  */
