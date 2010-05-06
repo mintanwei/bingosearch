97d4566518637f90dc4ba5d5f2d0b685dbdd6904
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-05-06 04:54:25 +0000

    go

--- crawler/crawler.cpp
+++ crawler/crawler.cpp
@@ -82,7 +82,7 @@ void Crawler::Download(const string& query, const size_t page, const string& dir
 
     if (curl) 
     {
-        string url("http://images.google.com.hk/images?");
+        string url("http://images.google.com/images?");
         url.append("q=");
         url.append(query);
         url.append("&start=");
