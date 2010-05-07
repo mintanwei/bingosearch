4e65fe50c1811dc93f20467bd32a89b2f4bfd51a
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-05-07 09:39:23 +0000

    go

--- move.sh
+++ move.sh
@@ -1,4 +1,4 @@
-sudo rm -rf front_end/index
-sudo rm -rf front_end/images
-sudo cp -r data/index front_end/
-sudo cp -r data/images front_end/
+sudo rm -rf /var/www/index
+sudo rm -rf /var/www/images
+sudo cp -r data/index /var/www/
+sudo cp -r data/images /var/www/
