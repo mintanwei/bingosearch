772af6395be88c480f57030ac94990f79ef34a8f
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-05-11 09:38:13 +0000

    go

--- trunk/move.sh
+++ trunk/move.sh
@@ -1,4 +1,6 @@
 sudo rm -rf /var/www/index
+sudo rm -rf /var/www/index_sift
 sudo rm -rf /var/www/images
 sudo cp -r data/index /var/www/
+sudo cp -r data/index_sift /var/www/
 sudo cp -r data/images /var/www/
