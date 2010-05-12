141e831b0c916ad793b9eb2e966f697c17f3d15f
jiansong.chao <jiansong.chao@0a3366de-0475-4a76-26f7-fec2d8f191d4>
2010-05-12 12:55:55 +0000

    go

---
+++ move.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+&move("data/index/", "front_end/index/");
+&move("data/index_sift/", "front_end/index_sift/");
+&move_dir("data/images/", "front_end/images/");
+
+sub move
+{
+	$from_dir = $_[0];
+	$to_dir = $_[1];
+	$file = "data/query_list";
+	open QUERY, $file or die "FAIL TO OPEN $file";
+	while (<QUERY>) {
+		chomp;
+		print "sudo cp $from_dir$_ $to_dir$_\n";
+		system("sudo cp $from_dir$_ $to_dir$_");
+	}
+	close QUERY;
+}
+
+sub move_dir
+{
+	$from_dir = $_[0];
+	$to_dir = $_[1];
+	$file = "data/query_list";
+	open QUERY, $file or die "FAIL TO OPEN $file";
+	while (<QUERY>) {
+		chomp;
+		print "sudo cp -r $from_dir$_ $to_dir$_\n";
+		system("sudo cp -r $from_dir$_ $to_dir$_");
+	}
+}
+
